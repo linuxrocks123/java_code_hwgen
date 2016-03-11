f50dd0ad0623674708a31a464e77ca125d370319
Patrick Simmons <linuxrocks123@netscape.net>
2016-03-11 02:10:01 -0500

    Update

--- code_hwgen.py
+++ code_hwgen.py
@@ -15,6 +15,9 @@ import sys
 if __name__=="__main__":
     flags = int(sys.argv[1])
 
+if len(sys.argv)==3:
+    random.setstate(eval(open(sys.argv[2]).read()))
+
 class JT:
     BOOLEAN=-1
     BYTE=0
@@ -45,7 +48,9 @@ class JE:
 
     def typecheck(self):
         if self.jtype==JT.CHAR:
-            self.value%=256
+            self.value%=65536
+            if self.value < 0:
+                self.value+=65536
         elif self.jtype==JT.BYTE:
             self.value%=256
             if self.value > 127:
@@ -73,7 +78,7 @@ class JE:
         self.typecheck()
 
 
-#Dictionary: (returns, param1, ...) : generator_func:
+#Dictionary: (preclevel, returns, param1, ...) : generator_func:
 rdict = {}
 NAMES = ("var","var2","x","y","z","pasta","pizza","banana","potato","bunny",
          "cecile", "steve","chong","van","aubrey", "candy","froyo","xkcd","gpf",
@@ -99,7 +104,7 @@ def prettyprint(subje2):
     if subje2.jtype==JT.STRING:
         subje2_value = subje2.value
     elif subje2.jtype==JT.CHAR:
-        subje2_value = chr(subje2.value) if subje2.value < 256 else chr(subje2.value%256)
+        subje2_value = chr(subje2.value) if subje2.value < 127 and subje2.value >= 32 else repr(subje2.value)
     elif subje2.jtype==JT.BOOLEAN:
         subje2_value = repr(subje2.value==True).lower()
     elif subje2.jtype >= 5:
@@ -124,7 +129,10 @@ class Rand_Exp:
 
     def gen_char_literal():
         x = random.randint(32,126)
-        return JE("'"+chr(x)+"'",[],x,JT.CHAR,0)
+        if x!=ord('\\'):
+            return JE("'"+chr(x)+"'",[],x,JT.CHAR,0)
+        else:
+            return JE("'\\\\'",[],x,JT.CHAR,0)
     add_to_rdict((0,JT.CHAR),gen_char_literal)
     
     def gen_int_literal():
@@ -164,7 +172,25 @@ class Rand_Exp:
     add_to_rdict((1,JT.BOOLEAN,JT.BOOLEAN),bool_not)
 
     def binary_arithmetic(op,subje1,subje2):
-        return JE("$1"+op+"$2",[subje1,subje2],eval(repr(subje1.value)+op+repr(subje2.value)),binop_promotion(subje1.jtype,subje2.jtype),PRECLEVEL[op])
+        if op not in ("/","%") or subje1.value > 0 and subje2.value > 0:
+            return JE("$1"+op+"$2",[subje1,subje2],eval(repr(subje1.value)+op+repr(subje2.value)),binop_promotion(subje1.jtype,subje2.jtype),PRECLEVEL[op])
+        else:
+            negation_needed=False
+            if subje1.value < 0:
+                subje1_value = -subje1.value
+                negation_needed = not negation_needed
+            else:
+                subje1_value = subje1.value
+            if subje2.value < 0:
+                subje2_value = -subje2.value
+                if op=='/':
+                    negation_needed = not negation_needed
+            else:
+                subje2_value = subje2.value
+            exp_val = eval(repr(subje1_value)+op+repr(subje2_value))
+            if negation_needed:
+                exp_val = -exp_val
+            return JE("$1"+op+"$2",[subje1,subje2],exp_val,binop_promotion(subje1.jtype,subje2.jtype),PRECLEVEL[op])            
     for op in PRECLEVEL:
         for jtype1 in (JT.BYTE,JT.CHAR,JT.SHORT,JT.INT,JT.LONG,JT.FLOAT,JT.DOUBLE):
             for jtype2 in (JT.BYTE,JT.CHAR,JT.SHORT,JT.INT,JT.LONG,JT.FLOAT,JT.DOUBLE):
@@ -257,11 +283,11 @@ def gen_complex_expr(level,jtype=-99):
         #subexpression is also of precedence 2.  If so, we must parenthesize it.
         if len(subexprs)==2 and precedence < subexprs[1].precedence or precedence==2 and subexprs[1].precedence==2:
             subexprs[1] = Rand_Exp.parentheses(subexprs[1])
-        elif len(subexprs)==1 and precedence==subexprs[0].precedence:
+        elif len(subexprs)==1 and precedence<=subexprs[0].precedence:
             subexprs[0] = Rand_Exp.parentheses(subexprs[0])
         elif len(subexprs)==2 and subexprs[1].precedence==1 and subexprs[1].jtype!=JT.BOOLEAN and type(generator)==functools.partial and generator.args[0]=='-':
             subexprs[1] = Rand_Exp.parentheses(subexprs[1])
-        elif len(subexprs)==2 and precedence==3 and subexprs[1].precedence==3 and subexprs[0].jtype==JT.STRING and subexprs[1].jtype!=JT.STRING:
+        elif len(subexprs)==2 and precedence==3 and subexprs[1].precedence==3 and (subexprs[0].jtype==JT.STRING and subexprs[1].jtype!=JT.STRING or type(generator)==functools.partial and generator.args[0]=='-'):
             subexprs[1] = Rand_Exp.parentheses(subexprs[1])
         
         #We also must check if our right subexpression is equal to 0 for / & %.
@@ -346,6 +372,16 @@ def create_new_var(varname,init_expr):
     to_append = symtab[init_expr.jtype]
     to_append.append((varname,JE(varname,[],init_expr.value,init_expr.jtype,0)))
 
+#Delete a variable from the symbol table (because it went out of scope)
+def delete_var(varname):
+    for jtype in symtab:
+        for i in range(len(symtab[jtype])):
+            if varname==symtab[jtype][i][0]:
+                del symtab[jtype][i]
+                if not len(symtab[jtype]):
+                    del symtab[jtype]
+                return
+
 #Reassign value of variable in symbol table
 def assign_var(varname,expr):
     for jtype in symtab:
@@ -358,8 +394,62 @@ def assign_var(varname,expr):
                 else:
                     return None
 
+#Helper function for flags==4 (and possibly others):
+#Handle single block (recursive)
+def flag4_block_helper(live,descent_level,max_live_descent_level,target_max_live_descent_level=2):
+    global symtab
+
+    random_word = NAMES[random.randint(0,len(NAMES)-1)]
+    question.write(" "*descent_level*4+"System.out.println(\""+random_word+"\");\n")
+    if live:
+        answer.write(random_word+"\n")
+
+    localsyms = []
+    max_val = random.randint(2,5)
+    for i in range(1,max_val+1):
+        #Do we do an if block here?
+        if descent_level < target_max_live_descent_level and (i==max_val and live and max_live_descent_level < target_max_live_descent_level or not random.randint(0,3)):
+            q = gen_complex_expr(2,JT.BOOLEAN)
+            while q.jtype!=JT.BOOLEAN:
+                q = gen_complex_expr(2,JT.BOOLEAN)
+            if live and q.value:
+                max_live_descent_level = max(descent_level+1,max_live_descent_level)
+            question.write(" "*descent_level*4+"if("+q.get_textual_representation()+")\n"+" "*descent_level*4+"{\n")
+            flag4_block_helper(live and q.value,descent_level+1,max_live_descent_level,target_max_live_descent_level)
+            question.write(" "*descent_level*4+"}\n")
+
+            #Do we do an else?
+            if i==max_val and live and not q.value and max_live_descent_level < target_max_live_descent_level or random.randint(0,1):
+                if live and not q.value:
+                    max_live_descent_level = max(descent_level+1,max_live_descent_level)
+                question.write(" "*descent_level*4+"else\n"+" "*descent_level*4+"{\n")
+                flag4_block_helper(live and not q.value,descent_level+1,max_live_descent_level,target_max_live_descent_level)
+                question.write(" "*descent_level*4+"}\n")
+        else: #no if; normal statement here
+            q = gen_complex_expr(3)
+            if q.jtype >= JT.BYTE and q.jtype < JT.DOUBLE:
+                jtype = random.randint(q.jtype,JT.DOUBLE)
+            else:
+                jtype = q.jtype
+            q.jtype = jtype
+            question.write(" "*descent_level*4)
+            if jtype not in symtab or not random.randint(0,3):
+                question.write(JT.TO_STR[jtype]+" ")
+                varname = get_new_varname()
+                create_new_var(varname,q)
+                localsyms.append(varname)
+            else:
+                varname = symtab[jtype][random.randint(0,len(symtab[jtype])-1)][0]
+                if live:
+                    assign_var(varname,q)
+            question.write(varname+"="+q.get_textual_representation()+";\n")
+    if descent_level:
+        for varname in localsyms:
+            delete_var(varname)
+
 question = open("question.txt","w")
 answer = open("answer.txt","w")
+open("state.python",'w').write(repr(random.getstate()))
 
 if flags==1:
     for i in range(1,11):
@@ -367,12 +457,17 @@ if flags==1:
         question.write(repr(i)+".\n"+q.get_textual_representation()+"\n\n")
         answer.write(JT.TO_STR[q.jtype]+" / "+prettyprint(q)+"\n")
 elif flags==2:
-    for _ in range(random.randint(1,15)):
+    for _ in range(random.randint(5,15)):
         q = gen_complex_expr(3)
         if q.jtype >= JT.BYTE and q.jtype < JT.DOUBLE:
             jtype = random.randint(q.jtype,JT.DOUBLE)
         else:
             jtype = q.jtype
+
+        if q.jtype < JT.FLOAT and jtype >= JT.FLOAT:
+            q.value = float(q.value)
+        q.jtype = jtype
+        
         if jtype not in symtab or not random.randint(0,3):
             question.write(JT.TO_STR[jtype]+" ")
             varname = get_new_varname()
@@ -388,3 +483,201 @@ elif flags==2:
     varsyms.sort()
     for var in varsyms:
         answer.write(var[0]+": "+prettyprint(var[1])+"\n")
+elif flags==3:
+    for i in range(1,6):
+        #Question Number
+        question.write(repr(i)+".\n")
+
+        #Return type
+        ret_type = random.randint(JT.STRING,7)
+        if ret_type==7:
+            question.write("void")
+        else:
+            question.write(JT.TO_STR[ret_type])
+        question.write(" ")
+        funcname = get_new_varname()
+        symtab[7]=[(funcname,None)]
+        question.write(funcname)
+        question.write("(")
+        args = random.randint(0,4)
+        argtypes = []
+        for j in range(args):
+            if j!=0:
+                question.write(", ")
+            paramtype = random.randint(JT.STRING,JT.DOUBLE)
+            argtypes.append(paramtype)
+            paramname = get_new_varname()
+            symtab[7].append((paramname,None))
+            question.write(JT.TO_STR[paramtype]+" "+paramname)
+        question.write(")")
+        question.write("\n...\n")
+        question.write(funcname+"(")
+        if ret_type==7:
+            tentative_answer="void"
+        else:
+            tentative_answer = JT.TO_STR[ret_type]
+        symtab={}
+        forced_valid=random.randint(0,1)
+        for j in range(args):
+            if j!=0:
+                question.write(", ")
+            if forced_valid:
+                valid = False
+                while not valid:
+                    expr = gen_complex_expr(2,argtypes[j])
+                    valid = not (expr.jtype > argtypes[j] or (expr.jtype < 0 or argtypes[j] < 0) and expr.jtype!=argtypes[j])
+            else:
+                if random.randint(0,1):
+                    expr = gen_complex_expr(2,argtypes[j])
+                else:
+                    expr = gen_complex_expr(2)
+                if expr.jtype > argtypes[j] or (expr.jtype < 0 or argtypes[j] < 0) and expr.jtype!=argtypes[j]:
+                    tentative_answer = "invalid"
+            question.write(expr.get_textual_representation())
+        question.write(");\n\n")
+        answer.write(tentative_answer+"\n")
+elif flags==4:
+    flag4_block_helper(True,0,0)
+    answer.write("\n")
+    varsyms = []
+    for jtype in symtab:
+        for var in symtab[jtype]:
+            varsyms.append(var)
+    varsyms.sort()
+    for var in varsyms:
+        answer.write(var[0]+": "+prettyprint(var[1])+"\n")
+elif flags==5:
+    for i in range(4):
+        nnum = random.randint(0,len(NAMES)-2)
+        question.write("String "+NAMES[nnum]+" = \""+NAMES[nnum+1]+"\";\n")
+        if i==0:
+            bg = random.randint(0,len(NAMES[nnum+1])/2)
+        elif i==2:
+            bg = random.randint(2,max(2,len(NAMES[nnum+1])/2))
+        elif i==1 or i==3:
+            bg = len(NAMES[nnum+1])-1-random.randint(0,len(NAMES[nnum+1])/2)
+        question.write("int i="+repr(bg)+";\n")
+        question.write("while(i")
+        if i==0 or i==2:
+            question.write("<"+repr(len(NAMES[nnum+1]))+")\n{\n")
+        elif i==1:
+            question.write(">=0)\n{\n")
+        elif i==3:
+            question.write(">0)\n{\n")
+        question.write("    System.out.print("+NAMES[nnum]+".charAt(i));\n")
+        if i==0 or i==1:
+            rn = random.randint(1,len(NAMES[nnum+1]))
+        elif i==2 or i==3:
+            rn = random.randint(2,3)
+        if i==0:
+            question.write("    i+="+repr(rn)+";\n")
+        elif i==1:
+            question.write("    i-="+repr(rn)+";\n")
+        elif i==2:
+            question.write("    i*="+repr(rn)+";\n")
+        elif i==3:
+            question.write("    i/="+repr(rn)+";\n")
+        question.write("}\n---\n\n")
+        x=bg
+        if i==0:
+            while(x<len(NAMES[nnum+1])):
+                answer.write(NAMES[nnum+1][x])
+                x+=rn
+        elif i==1:
+            while(x>=0):
+                answer.write(NAMES[nnum+1][x])
+                x-=rn
+        elif i==2:
+            while(x<len(NAMES[nnum+1])):
+                answer.write(NAMES[nnum+1][x])
+                x*=rn
+        elif i==3:
+            while(x>0):
+                answer.write(NAMES[nnum+1][x])
+                x/=rn
+        answer.write("\n")
+elif flags==6:
+    for i in range(4):
+        nnum = random.randint(0,len(NAMES)-2)
+        question.write("String "+NAMES[nnum]+" = \""+NAMES[nnum+1]+"\";\n")
+        if i==0:
+            bg = random.randint(0,len(NAMES[nnum+1])/2)
+        elif i==2:
+            bg = random.randint(2,max(2,len(NAMES[nnum+1])/2))
+        elif i==1 or i==3:
+            bg = len(NAMES[nnum+1])-1-random.randint(0,len(NAMES[nnum+1])/2)
+        question.write("for(int i="+repr(bg)+"; i")
+        if i==0 or i==2:
+            question.write("<"+repr(len(NAMES[nnum+1]))+"; ")
+        elif i==1:
+            question.write(">=0; ")
+        elif i==3:
+            question.write(">0; ")
+
+        if i==0 or i==1:
+            rn = random.randint(1,len(NAMES[nnum+1]))
+        elif i==2 or i==3:
+            rn = random.randint(2,3)
+        if i==0:
+            question.write("i+="+repr(rn)+")\n")
+        elif i==1:
+            question.write("i-="+repr(rn)+")\n")
+        elif i==2:
+            question.write("i*="+repr(rn)+")\n")
+        elif i==3:
+            question.write("i/="+repr(rn)+")\n")
+        question.write("    System.out.print("+NAMES[nnum]+".charAt(i));\n")
+        question.write("\n---\n\n")
+        x=bg
+        if i==0:
+            while(x<len(NAMES[nnum+1])):
+                answer.write(NAMES[nnum+1][x])
+                x+=rn
+        elif i==1:
+            while(x>=0):
+                answer.write(NAMES[nnum+1][x])
+                x-=rn
+        elif i==2:
+            while(x<len(NAMES[nnum+1])):
+                answer.write(NAMES[nnum+1][x])
+                x*=rn
+        elif i==3:
+            while(x>0):
+                answer.write(NAMES[nnum+1][x])
+                x/=rn
+        answer.write("\n")
+elif flags==7:
+    for i in range(1,10):
+        #Question Number
+        question.write(repr(i)+".  ")
+        answer.write("public static ")
+        
+        #Return type
+        ret_type = random.randint(JT.STRING,7)
+        if ret_type==7:
+            question.write("A function returning no value")
+            answer.write("void")
+        else:
+            question.write("A function returning "+JT.TO_STR[ret_type])
+            answer.write(JT.TO_STR[ret_type])
+        answer.write(" ")
+        funcname = get_new_varname()
+        symtab[7]=[(funcname,None)]
+        question.write(", named "+funcname)
+        answer.write(funcname)
+        answer.write("(")
+        args = random.randint(0,4)
+        question.write(", and taking "+repr(args)+" parameters.")
+        argtypes = []
+        for j in range(args):
+            if j!=0:
+                answer.write(", ")
+            question.write("  ")
+            paramtype = random.randint(JT.STRING,JT.DOUBLE)
+            argtypes.append(paramtype)
+            paramname = get_new_varname()
+            symtab[7].append((paramname,None))
+            question.write("Parameter "+repr(j+1)+" should be named "+paramname+" and should be of type "+JT.TO_STR[paramtype]+".")
+            answer.write(JT.TO_STR[paramtype]+" "+paramname)
+        question.write("\n\n")
+        answer.write(")\n")
